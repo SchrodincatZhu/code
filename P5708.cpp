c45d554d6f85294ed0586a57a5205cd4b8611d03
Schrodincat <Schrodincat@LAPTOP-HALNBOPG>
2025-09-01 21:27:30 +0800

    P5708

---
+++ P5708.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    float a,b,c;
+    cin>>a>>b>>c;
+    float p=0.5*(a+b+c);
+    float S=sqrt(p*(p-a)*(p-b)*(p-c));
+    printf("%.1f",S);
+    return 0;
+}
