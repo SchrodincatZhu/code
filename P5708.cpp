b00be4efc00e3d49afce174d8a8ef1142f2ccc8f
Schrodincat <Schrodincat@LAPTOP-HALNBOPG>
2025-09-01 23:21:49 +0800

    格式修改

--- P5708.cpp
+++ P5708.cpp
@@ -1,11 +1,12 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
-int main(){
-    float a,b,c;
-    cin>>a>>b>>c;
-    float p=0.5*(a+b+c);
-    float S=sqrt(p*(p-a)*(p-b)*(p-c));
-    printf("%.1f",S);
+int main()
+{
+    float a, b, c;
+    cin >> a >> b >> c;
+    float p = 0.5 * (a + b + c);
+    float S = sqrt(p * (p - a) * (p - b) * (p - c));
+    printf("%.1f", S);
     return 0;
 }
