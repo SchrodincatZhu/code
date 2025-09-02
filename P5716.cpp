31e9ec196e7021181044fca2dd26e8702774e401
Schrodincat <Schrodincat@LAPTOP-HALNBOPG>
2025-09-02 12:12:37 +0800

    P5716

---
+++ P5716.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int y, m;
+    cin >> y >> m;
+    bool year = 0;
+
+    if (y % 4 == 0 && y % 100 != 0)
+    {
+        year = 1;
+    }
+    else if (y % 400 == 0)
+    {
+        year = 1;
+    }
+    if (year == 0 && m == 2)
+    {
+        cout << 28;
+    }
+    else if (year == 1 && m == 2)
+    {
+        cout << 29;
+    }
+    else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+    {
+        cout << 31;
+    }
+    else
+    {
+        cout << 30;
+    }
+    return 0;
+}
