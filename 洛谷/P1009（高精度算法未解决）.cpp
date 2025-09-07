8be2f496ce67eb1f8bca523e5d219ae3469d4096
Schrodincat <Schrodincat@LAPTOP-HALNBOPG>
2025-09-07 20:53:40 +0800

    新建文件夹

---
+++ 洛谷/P1009（高精度算法未解决）.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    string a[200];
+    for (int i = 0; i < n; i++)
+    {
+        a[i] =
+    }
+    return 0;
+}
