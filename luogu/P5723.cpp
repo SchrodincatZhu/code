17ea45c0d20629b9e3611726f048586f787348ea
Schrodincat <Schrodincat@LAPTOP-HALNBOPG>
2025-09-08 12:22:36 +0800

    P5723

--- luogu/P5723.cpp
+++ luogu/P5723.cpp
@@ -1,7 +1,39 @@
+
 #include <iostream>
 using namespace std;
 int main()
 {
+    int max;
+    cin >> max;
+    int number = 0;
+    int sum = 0;
+
+    if (max == 2)
+    {
+        cout << 2 << endl
+             << 1<<endl;;
+        return 0;
+    }
+
+    for (int i = 2; i <= max; i++)
+    {
+        bool a = 0;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                a = 1;
+                break;
+            }
+        }
+        if (a == 0 && sum + i <= max)
+        {
+            number++;
+            cout << i << endl;
+            sum += i;
+        }
+    }
+    cout << number<<endl;
 
     return 0;
 }
