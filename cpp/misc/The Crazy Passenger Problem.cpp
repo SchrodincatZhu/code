8bc338831b876647ae21d1586646bed13afe9c1c
Schrodincat <Schrodincat@LAPTOP-HALNBOPG>
2026-07-10 19:53:02 +0800

    The Crazy Passenger Problem

---
+++ cpp/misc/The Crazy Passenger Problem.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <random>
+using namespace std;
+int main()
+{
+
+    const int PassengerNumber = 200;
+    const int simulations = 100000;
+    int SuccessCount = 0;
+
+    random_device seed;
+    mt19937 gen(seed());
+
+    for (int sim = 0; sim < simulations; ++sim)
+    {
+
+        vector<bool> SeatOccupied(PassengerNumber + 1, false);
+
+        uniform_int_distribution<int> DistFirst(1, PassengerNumber);
+        int FirstChoice = DistFirst(gen);
+        SeatOccupied[FirstChoice] = true;
+
+        for (int i = 2; i < PassengerNumber; ++i)
+        {
+            if (SeatOccupied[i] == false)
+            {
+
+                SeatOccupied[i] = true;
+            }
+            else
+            {
+
+                vector<int> EmptySeats;
+                for (int j = 1; j <= PassengerNumber; ++j)
+                {
+                    if (SeatOccupied[j] == false)
+                    {
+                        EmptySeats.push_back(j);
+                    }
+                }
+
+                uniform_int_distribution<int> DistEmpty(0, EmptySeats.size() - 1);
+                int pick = EmptySeats[DistEmpty(gen)];
+                SeatOccupied[pick] = true;
+            }
+        }
+
+        if (SeatOccupied[PassengerNumber] == false)
+        {
+            SuccessCount++;
+        }
+    }
+
+    cout << "模拟次数: " << simulations << endl;
+    cout << "第 " << PassengerNumber << " 个人坐在自己位置上的概率: "
+         << (double)SuccessCount / simulations * 100 << "%" << endl;
+
+    return 0;
+}
